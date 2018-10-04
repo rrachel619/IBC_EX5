ea0dc53f188f88c7b9b137e8619811ba0a30760d
rachel <rrodri22@nd.edu>
2018-10-04 17:38:05 -0400

    newfile.sh

--- newfile.sh
+++ newfile.sh
@@ -1,3 +1,3 @@
 # Usage: creating file with gender and years experience in separate columns, sorted by gender with pairings still intact
 
-cat wages.csv | cut -d "," -f 1,2 | uniq | tr "," " " | sort -t " " -k1 | sort -t " " -k2n > newfile.csv
+cat wages.csv| grep -v gender | cut -d "," -f 1,2 | uniq | tr "," " " | sort -t " " -k1,1d -k2,2n > newfile.csv
